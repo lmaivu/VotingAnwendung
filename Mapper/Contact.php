6b4c874fb81fe1ce47adf704b8771be7fcf35dc2
lenafranzisca <lenabogunovic@outlook.de>
2016-04-19 10:25:59 +0200

    Änderungen Kontaktseite

--- Mapper/Contact.php
+++ Mapper/Contact.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: L.Mai
- * Date: 12.04.2016
- * Time: 17:16
- */
+<link rel="stylesheet" href="bootstrap_lena.css">
+
+<div class="main">
+    <h1 id="title"> Kontakt </h1>
+    <h4> HdM Stuttgart<br>
+        Nobelstra&szlige 10<br>
+        70569 Stuttgart<br>
+        Telefon: 0711 892310<br>
+    </h4>
+</div>
+
+<div class="footer">
+    <p> &copy iwillsurvey.de </p>
+</div>

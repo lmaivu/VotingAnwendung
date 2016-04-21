3e2100ef788f76b461aca7644b2a2c7ec973e372
lenafranzisca <lenabogunovic@outlook.de>
2016-04-21 10:34:27 +0200

    Änderungen 21.4.

--- Mapper/Contact.php
+++ Mapper/Contact.php
@@ -1,4 +1,68 @@
-<link rel="stylesheet" href="bootstrap_lena.css">
+<head>
+    <meta charset="UTF-8">
+    <title> I will survey - Voting </title>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!--
+     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <!-- JQuery einbinden -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>﻿
+</head>
+
+<body>
+
+<!--- Navbar Bginn --->
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    </div>
+
+    <!-- Logo-->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+        <a href="#" class="navbar-brand">I-will-Survey</a>
+    </div>
+
+
+    <!-- Menü-Items -->
+    <div class="collapse navbar-collapse" id="mainNavBar">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Startseite</a></li>
+            <li> <a href="#">Kontakt</a></li>
+            <li> <a href="#">Impressum</a></li>
+
+            <!-- Dropdown-Menü -->
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hallooo <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li> <a href="#">Mai</a></li>
+                    <li> <a href="#">Lena</a></li>
+                    <li> <a href="#">Luke</a></li>
+
+                </ul>
+            </li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li> <a href="#">Logout</a></li>
+        </ul>
+
+    </div>
+</nav>
+
+<!--- Navbar Ende --->
+
+<link rel="stylesheet" href="../css/bootstrap_lena.css">
 
 <div class="main">
     <h1 id="title"> Kontakt </h1>
@@ -12,3 +76,4 @@
 <div class="footer">
     <p> &copy iwillsurvey.de </p>
 </div>
+

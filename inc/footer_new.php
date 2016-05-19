cfb699a3b9727915f7e77dd35509da731ec38154
lneuffer <lukasneuffer@web.de>
2016-05-19 06:40:10 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/deployment.xml

--- inc/footer_new.php
+++ inc/footer_new.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lukas
- * Date: 17.05.16
- * Time: 15:28
- */
+
+
+<footer class="footer">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-5 bottom_copyright" style="padding: 0 15px; border: 0; background:transparent;">
+                <p class="pull-left" style="font-size:11px; margin-top: 10px;">
+                    Copyright by MaLeLu 2016 - all rights reserved
+                    -
+                </p>
+            </div>
+            <div class="col-xs-12 col-sm-7 bottom_navi" style="padding: 0 15px; border: 0; background:transparent;">
+                <ul class="nav nav-pills pull-right" style="font-size:11px; margin-top: 0px;">
+                    <li><a href="index.html"><i class="fb fa fa-lg fa-home"></i>HOME</a></li>
+                    <li><a href="#" target="_top" title="go back" onclick="history.back();return false;" class="link-back"><i class="fb fa fa-lg fa-arrow-left"></i>BACK</a></li>
+                    <li><a href="index.html#top" class="link-top"><i class="fb fa fa-lg fa-arrow-up"></i>TOP</a></li>
+                    <li><a href="login-logout.html"><i class="fb fa fa-lg fa-sign-in"></i>FElogIN</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</footer>

679f7f8a55bb5018d98c5507a3295b96d773c03d
lneuffer <lukasneuffer@web.de>
2016-05-19 07:03:06 +0200

    1

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,23 @@
-<div class ="navbar navbar-default navbar-fixed-bottom">
+
+
+<footer class="footer">
+
     <div class="container">
-
-        <a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Contact.php" class="navbar-btn btn-default btn pull right">Kontakt</a>
-        <a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Impressum.php" class="navbar-btn btn-default btn pull right">Impressum</a>
-
+        <div class="row">
+            <div>
+                <p class="pull-left" style="font-size:11px; margin-top: 10px;">
+                    © by MaLeLu - 2016
+                </p>
+            </div>
+            <div>
+                <ul class="nav nav-pills pull-right" style="font-size:11px; margin-top: 2px;">
+                    <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Contact.php">Kontakt</a></li>
+                    <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Impressum.php">Impressum</a></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
     </div>
-</div>
+
+</footer>

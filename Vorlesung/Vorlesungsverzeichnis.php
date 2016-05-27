ed3c62e598406afe8fce78f4a6053763f6512c83
lmaivu <l.maivu@hotmail.de>
2016-05-27 15:17:02 +0200

    Mais Änderungen

--- Vorlesung/Vorlesungsverzeichnis.php
+++ Vorlesung/Vorlesungsverzeichnis.php
@@ -1,4 +1,4 @@
-<!-- erstellte Votingdaten werden hier mithilfe von GET übermittelt
+<!-- erstellte Votingdaten werden hier mithilfe von GET ï¿½bermittelt
 Darstellung der Daten in Tabellenform-->
 
 <?php
@@ -21,7 +21,7 @@ require_once("../Mapper/VotingManager.php");
 require_once("../VotingVorlesung/VotingVorlesungManager.php");
 
 
-//eingetragenes Voting wird mittels GET Methode übermittelt
+//eingetragenes Voting wird mittels GET Methode ï¿½bermittelt
 //neues Objekt der Klasse initiiert, um auf bestimmte Methoden zuzugreifen
 $vorlesung_ID = (int)htmlspecialchars($_GET["vorlesung_ID"], ENT_QUOTES, "UTF-8");
 $vorlesungManager = new VorlesungManager();
@@ -42,7 +42,6 @@ $vorlesung = $vorlesungManager->findById($vorlesung_id);
 <div class="container">
     <div class="jumbotron">
         <h1>Vorlesungsverzeichnis</h1>
-        <?php echo ($voting->name." / ".date("d.m.Y", strtotime($voting->datum))); ?>
     </div>
 
     <?php
@@ -59,15 +58,15 @@ $vorlesung = $vorlesungManager->findById($vorlesung_id);
         <?php
         foreach ($liste as $vorlesung) {
             echo "<tr>";
-            echo "<td>$vorlesung->vorlesung_ID</td>";
+            echo "<td>$vorlesung->vo9rlesung_ID</td>";
             echo "<td>$vorlesung->vorlesung_name</td>";
              ?>
             <td> <a type="button" class="btn btn-info" href="VorlesungRead.php" role="button">anzeigen</a> </td>
             <td> <a type="button" class="btn btn-primary" href="VorlesungUpdate_form.php" role="button">bearbeiten</a> </td>
             <td> <a type="button" class="btn btn-primary" href="VorlesungDelete.php" role="button">l&oumlschen</a> </td>
             <td> <a type="button" class="btn btn-primary" href="../Voting/VotingRead.php" role="button">Voting anzeigen</a>
-                <!--überprüfen!!!-->
-            <?php //nicht nötig echo "<td><a href='LeserUnconnect_do.php?notiz_id=$notiz->id&leser_id=$leser->id' class='btn btn-info btn-danger btn-xs' >Verbindung lösen</a>";
+                <!--ï¿½berprï¿½fen!!!-->
+            <?php //nicht nï¿½tig echo "<td><a href='LeserUnconnect_do.php?notiz_id=$notiz->id&leser_id=$leser->id' class='btn btn-info btn-danger btn-xs' >Verbindung lï¿½sen</a>";
             echo "<td></td>";
             echo "</tr>";
         } }

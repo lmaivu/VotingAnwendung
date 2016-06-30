91f1ddec7cf95bfb49532d41f0134d2a933067aa
lneuffer <lukasneuffer@web.de>
2016-06-30 11:09:46 +0200

    1

--- qr_code/php/qr_voting.php
+++ qr_code/php/qr_voting.php
@@ -1,7 +1,3 @@
-<!-- <?php
-echo "<img src='qr_img.php?d=I-Will-Survey'>";
-?> -->
-
 
 <?php
 echo "<img src='qr_img.php?d=<a href=\"https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Voting_Studies_Test.php\"></a>'>";

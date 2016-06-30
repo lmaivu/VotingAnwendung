1f0cae4e7ecf45ac486bf0097204bc290247884a
lmaivu <l.maivu@hotmail.de>
2016-06-30 12:05:08 +0200

    Mais Änderungen

---
+++ Mapper/DozentPasswort_Form.php
@@ -0,0 +1,52 @@
+<?php
+include("../inc/session_check.php");
+?>
+
+
+<html>
+
+<?php include("../inc/head.php"); ?>
+
+<body>
+
+<?php include("../inc/navbar.php"); ?>
+
+<div class="container">
+
+    <h2>Hier k&oumlnnen Sie Ihr Passwort &aumlndern</h2><br />
+
+    <form class="form-horizontal" role="form" action="DozentPasswort_Do.php" method="post">
+
+        <div class="form-group">
+            <label for="password" class="control-label col-sm-2">Neues Passwort:</label>
+            <div class="col-sm-4">
+                <input type="password" class="form-control" name="hash" id="hash">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="password2" class="control-label col-sm-2">Neues Passwort wiederholen:</label>
+            <div class="col-sm-4">
+                <input type="password" class="form-control" name="hash2" id="hash2">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-4">
+                <button type="submit" class="btn btn-default">best&aumltigen</button>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-sm-5"</label>
+            <div class="col-sm-6">
+                <input type="hidden" value="<?php echo htmlspecialchars($Dozent->Dozent_ID); ?>" class="form-control" name="Dozent_ID" id="Dozent_ID" readonly>
+            </div>
+        </div>
+
+    </form>
+
+
+</body>
+</html>
+    
--- Mapper/Dozent_Passwort.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: L.Mai
- * Date: 30.06.2016
- * Time: 11:46
- */

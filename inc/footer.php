ecff032246cb953a63478e9748c3a028b02a392e
lneuffer <lukasneuffer@web.de>
2016-07-02 12:19:31 +0200

    1

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer wird auf jeder Seite eingebunden -->
 
-<footer id="footer">
+<!-- <footer id="footer">
 
     <div class="container" style="background-color: #ebedf2">
         <div class="row">
@@ -18,4 +18,20 @@
         </div>
     </div>
 
-</footer>
+</footer> -->
+
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <div>
+        <p class="pull-left" style="font-family: 'Open Sans', sans-serif; font-size:11px; margin-top: 10p">
+            © by MaLeLu - 2016
+        </p>
+    </div>
+    <div class="container">
+        <ul class="nav nav-pills pull-right" style="font-size:11px; margin-top: 2px;">
+            <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Contact.php">Kontakt</a></li>
+            <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Impressum.php">Impressum</a></li>
+        </ul>
+    </div>
+</nav>
+
+

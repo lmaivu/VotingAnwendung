23272619a59409959add5cfecf4857ed8c7ce7ec
lneuffer <lukasneuffer@web.de>
2016-07-02 13:44:29 +0200

    1

--- inc/sticky_footer.php
+++ inc/sticky_footer.php
@@ -3,16 +3,8 @@
     <div class="container-fluid">
         <p class="navbar-text pull-left"> © by MaLeLu - 2016 </p>
         <ul class="nav navbar-nav navbar right">
-             <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Contact.php">Kontakt</a></li>
+            <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Contact.php">Kontakt</a></li>
             <li><a href="https://mars.iuk.hdm-stuttgart.de/~lv018/Mapper/Impressum.php">Impressum</a></li>
         </ul>
     </div>
 </nav>
-
-
-
-
-
-
-
-

293ccf5fbd37ae994d44e48970339057b4df2994
lmaivu <l.maivu@hotmail.de>
2016-07-02 12:15:30 +0200

    Mais Änderungen

--- Voting/VotingStudent_Do.php
+++ Voting/VotingStudent_Do.php
@@ -14,13 +14,14 @@ $VotingManager = new VotingManager();
 $Voting = $VotingManager->findById($Voting_ID);
 
 //-------------Cookie checken, hat der Student schon gevoted?--------------------------
-if (isset ($_COOKIE["voted"] )) {
+if (isset ($_COOKIE["$Voting->Voting_ID"] )) {
     echo "Sie haben bereits erfolgreich Ihre Voting-Stimme abgegeben. Jeder Student kann nur ein Mal abstimmen."; ?>
-    <a href="#"> Hier können Sie das Ergebnis anschauen. </a>
+    <a href="#"> Hier k&oumlnnen Sie das Ergebnis anschauen. </a>
     <?php
 }
     elseif (isset($_POST['a_Student'])){
-        $a_Student = "UPDATE Voting SET a_Student=a_Student+1 WHERE Voting_ID=$Voting_ID";
+        $Voting = $VotingManager->updateA($Voting);
+        echo $Voting;
 
     }
     elseif(isset($_POST['b_Student'])){
@@ -37,20 +38,20 @@ if (isset ($_COOKIE["voted"] )) {
     $d_Student = "UPDATE Voting SET d_Student=d_Student+1 WHERE Voting_ID=$Voting_ID";
 
 }
-    elseif (!isset ($_COOKIE["voted"] )) {
+    elseif (!isset ($_POST['Antwort_A']) && ($_POST['Antwort_B']) && ($_POST['Antwort_C']) && ($_POST['Antwort_D'])) {
         echo "Fehler beim Abstimmen. Probiere Sie es erneut!";}
 
 
 
 $Voting = $VotingManager->countVote($Voting_ID);
 
-
 $a = $Voting->a_Student;
 $b = $Voting->b_Student;
 $c = $Voting->c_Student;
 $d = $Voting->d_Student;
 
-$count = $a+$b+$c+$d;
+//$count = $a+$b+$c+$d;
+$count= 5;
 
 $Prozent_a = round($a*100/$count) . "%";
 $Prozent_b = round($b*100/$count) . "%";

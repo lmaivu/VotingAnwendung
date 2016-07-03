b5902f4fb0e7950a641eca4052e3fb06458c24b2
lenafranzisca <lenabogunovic@outlook.de>
2016-07-03 16:31:50 +0200

    VotingCreate_form bearbeitet

--- Mapper/qr_code.php
+++ Mapper/qr_code.php
@@ -1,3 +1,5 @@
+<!-- QR-Code Generierung -->
+
 <?php
 
 include("../inc/qrlib.php");
@@ -10,7 +12,7 @@ $param = (int)htmlspecialchars($_GET["Voting_ID"], ENT_QUOTES, "UTF-8");
 ob_start("callback");
 
 // here DB request or some processing
-$codeText = 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Voting_Studies_Test2.php?Voting_ID='.$param;
+$codeText = 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Voting_Studies_Test2.php?Voting_ID='.$Voting_ID;
 
 // end of processing here
 $debugLog = ob_get_contents();
@@ -21,4 +23,20 @@ QRcode::png($codeText);
 
 ?>
 
+<!-- Ende QR-Code -->
+
+<!-- bit.ly Generierung -->
+<?php
+
+    $login = 'o_7dgmnsdmrq';
+    $key = 'R_f9535d857c614860a4dd5bf4d7e80b39';
+    $longurl = 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Voting_Studies_Test2.php?Voting_ID='.$Voting_ID;
+
+    $url = 'http://api.bit.ly/shorten?version=2.0.1&longUrl='.$longurl.'&login='.$login.'&apiKey='.$key.'';
+
+    $page = file_get_contents($url);
+    $result = json_decode($page);
+?>
+
+    <p <?php $result->{'results'}->{$longurl}->{'shortUrl'} ?> >
 

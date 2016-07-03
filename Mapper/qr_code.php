61256c1b4bdbaf52b89b32e573ef6328c9325919
lmaivu <l.maivu@hotmail.de>
2016-07-03 16:52:23 +0200

    Mais Änderungen

--- Mapper/qr_code.php
+++ Mapper/qr_code.php
@@ -11,6 +11,7 @@ $Voting_ID = (int)htmlspecialchars($_GET["Voting_ID"], ENT_QUOTES, "UTF-8");
 
 ob_start("callback");
 
+
 // here DB request or some processing
 $codeText = 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Voting_Studies_Test.php?Voting_ID='.$Voting_ID;
 

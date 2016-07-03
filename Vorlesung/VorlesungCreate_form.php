36a05a4e2fc0ddaf98164cfeecd913f01929fae6
lenafranzisca <lenabogunovic@outlook.de>
2016-07-03 15:27:10 +0200

    Änderungen CSS

--- Vorlesung/VorlesungCreate_form.php
+++ Vorlesung/VorlesungCreate_form.php
@@ -20,28 +20,29 @@ include "../inc/sticky_footer.php";
     <h2>Vorlesung erstellen</h2>
 
     <form class="form-horizontal" role="form" action="VorlesungCreate_do.php" method="post">
-
+     <br>
         <div class="form-group">
-            <label class="control-label col-sm-2" for="vorlesung_name">Name:</label>
-            <div class="col-sm-10">
+            <label class="control-label col-sm-2" for="vorlesung_name">Name: </label>
+            <div class="col-sm-8">
                 <input type="text" class="form-control" name="Vorlesung_Name" id="Vorlesung_Name" placeholder="Name der Vorlesung">
             </div>
         </div>
 
-
-
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">hinzuf&uuml;gen</button>
+         <br>
+     <div>
+        <div class="form-group" style= "alignment: center">
+            <div class="col-sm-offset-2 col-sm-8">
+                <button style='background-color: #Cdbfa5; border-color: white; color: white' type="submit" class="btn btn-default">Vorlesung hinzufügen</button>
+                <input style='background-color: #534532; border-color: white; color: white;' type="button" class="btn btn-default" value="Zurück" onClick="history.back()">
             </div>
         </div>
 
+
+
         <div class="form-group">
             <label class="control-label col-sm-5"> </label>
-            <div class="col-sm-6">
+            <div class="col-sm-8">
                 <input type="hidden" value="<?php echo htmlspecialchars($dozent->Dozent_ID); ?>" class="form-control" name="Dozent_ID" id="Dozent_ID" readonly>
-
-
             </div>
         </div>
     </form>

fe658af6662d59d94b7460b1fab00e3ebc5ad077
lenafranzisca <lenabogunovic@outlook.de>
2016-07-04 17:32:18 +0200

    VotingCreate_form bearbeitet

--- Mapper/Link.php
+++ Mapper/Link.php
@@ -1,6 +1,9 @@
 <?php
-include "../inc/head.php"
-
+include "../inc/head.php";
+require_once("../Vorlesung/Vorlesung.php");
+require_once("../Mapper/VorlesungManager.php");
+require_once ("../Voting/Voting.php");
+require_once("../Mapper/VotingManager.php");
 ?>
 
 <!DOCTYPE html>
@@ -9,14 +12,17 @@ include "../inc/head.php"
 
 
 <div class="info">
-    <p style="font-family: 'Open Sans', sans-serif"> Über diese QR-Code oder den Link gelangen Sie<br>
+    <p style="font-family: 'Open Sans', sans-serif"> Über diesen QR-Code oder den Link gelangen Sie<br>
         zur Abstimmung!</p>
     <?php
         $Voting_ID = (int)htmlspecialchars($_GET["Voting_ID"], ENT_QUOTES, "UTF-8");
+        $Einschreibeschlussel = (int)htmlspecialchars($_GET["Einschreibeschlussel"], ENT_QUOTES, "UTF-8");
         echo '<img src="../Mapper/qr_code.php?Voting_ID=' . $Voting_ID . '">';?>
         <br>
         <br> <?php
-        echo 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Schluesselabfrage_form.php?Voting_ID=' . $Voting_ID . '';
+        echo 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Schluesselabfrage_form.php?Voting_ID=' . $Voting_ID . '';?>
+        <br> <?php
+        echo 'Einschreibeschlüssel: ' . $Voting->Einschreibeschlussel . '';
     ?>
 </div>
 

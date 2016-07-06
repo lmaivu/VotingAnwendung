46b0fad1f3c2da698744f42094774cdf58d2e6e5
lneuffer <lukasneuffer@web.de>
2016-07-06 17:20:44 +0200

    1

--- Mapper/qr_code.php
+++ Mapper/qr_code.php
@@ -1,47 +1,34 @@
 <?php
-
 include "../inc/head.php";
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <body>
-
 <!-- QR-Code Generierung -->
 <div class="info">
 
     <p style="font-family: 'Open Sans', sans-serif"> Über diese QR-Code oder den Link gelangen Sie<br>
         zur Abstimmung!</p>
-
     <?php
 
     include("../inc/qrlib.php");
 
     $Voting_ID = (int)htmlspecialchars($_GET["Voting_ID"], ENT_QUOTES, "UTF-8");
 
-    // we need to be sure ours script does not output anything!!!
-    // otherwise it will break up PNG binary!
-
     ob_start("callback");
-
-
-    // here DB request or some processing
+    
     $codeText = 'https://mars.iuk.hdm-stuttgart.de/~lv018/Voting/Schluesselabfrage_form.php?Voting_ID='.$Voting_ID;
 
-    // Übergabe an Link.php //
-
-    // end of processing here
+    // Ende
     $debugLog = ob_get_contents();
     ob_end_clean();
 
 
-    // outputs image directly into browser, as PNG stream
+    // Das Bild wird als PNG übergeben 
     QRcode::png($codeText);
 
     ?>
 </div>
 <!-- Ende QR-Code -->
-
 </body>
 </html>
